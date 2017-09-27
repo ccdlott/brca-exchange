1d4d7a26ccf1c1926f14c61136bb9d72ff0a3cf3
Chloe Dlott <ccdlott@gmail.com>
2017-09-27 00:06:03 +0000

    Simplifies tests for init_proband and add_parents, adds variable indexes to improve code readability

--- case_level/simulation/test_pedigree_case_sims.py
+++ case_level/simulation/test_pedigree_case_sims.py
@@ -1,43 +1,36 @@
-import pytest
 import unittest
 from case_sims import Pedigree, year
 
-class test_pedigree_methods(unittest.TestCase):
+famID_idx = 0
+name_idx = 1
+target_idx = 2
+indivID_idx = 3
+fathID_idx = 4
+mothID_idx = 5
+sex_idx = 6
+twin_idx = 7
+dead_idx = 8
+age_idx = 9
+birthYear_idx = 10
+brCa_1_idx = 11
+brCa_2_idx = 12
+ovCa_idx = 13
+proCa_idx = 14
+panCa_idx = 15
+gTest_idx = 16
+mutn_idx = 17
+ashkn_idx = 18
+ER_idx = 19
+PR_idx = 20
+HER2_idx = 21
+CK14_idx = 22
+CK56_idx = 23
 
+class test_pedigree_methods(unittest.TestCase):
+    
     def setUp(self):
 
-        self.variable_defs = {
-            "FamID" : 0,
-            "Name" : 1,
-            "Target" : 2,
-            "IndivID" : 3,
-            "FathID" : 4,
-            "MothID" : 5,
-            "Sex" : 6,
-            "Twin" : 7,
-            "Dead" : 8,
-            "Age" : 9,
-            "Birth Year" : 10,
-            "BrCa_1" : 11,
-            "BrCa_2" : 12,
-            "OvCa" : 13,
-            "ProCa" : 14,
-            "PanCa" : 15,
-            "G Test" : 16,
-            "Mutn" : 17,
-            "Ashkn" : 18,
-            "ER" : 19,
-            "PR" : 20,
-            "HER2" : 21,
-            "CK14" : 22,
-            "CK56" : 23
-            }
-        
-        self.variable_zero = ["Twin", "PanCa", "ProCa", "Ashkn", "ER", "PR", "HER2", "CK14", "Ck56"]
-
         self.gender = ["M", "F"]
-        
-        self.pro_age_range = range(20,66)
 
         self.person = [1, 1, 0, 1, 0, 0, "M", 0, 0, 30, 1987, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -46,104 +39,74 @@ class test_pedigree_methods(unittest.TestCase):
     def test_init_proband(self):
         '''
         Tests that: 
-        1. Variables always set to zero in variable_zero list are equal to zero
-        2. FamID, Name, and IndivID are greater or equal to 1
-        3. Name and IndivID are equal
-        4. Target is set to 1
-        5. Sex is set to either "M" or "F"
-        6. Age is in age range specified in init_proband
-        7. Birth year is set correctly based on age
-        8. All other variables are equal to zero
-        9. Tests age edge cases
+        1. Target is equal to 1
+        2. Age is in age range specified in init_proband
         '''
         ped = Pedigree()
         proband = ped.init_proband()
-        for name, index in self.variable_defs.iteritems():
-            if name in self.variable_zero:
-                self.assertEqual(0, proband[index])
-            elif name == "FamID":
-                self.assertGreaterEqual(proband[index], 1)
-            elif name == "Name":
-                self.assertGreaterEqual(proband[index], 1)
-            elif name == "IndivID":
-                self.assertGreaterEqual(proband[index], 1)
-                self.assertEqual(proband[index], proband[index-2])
-            elif name == "Target":
-                self.assertEqual(1, proband[index])
-            elif name == "Sex":
-                self.assertIn(proband[index], self.gender)
-            elif name =="Age":
-                self.assertIn(proband[index], self.pro_age_range)
-            elif name == "Birth Year":
-                expected_birth_year = int(year) - int(proband[9])
-                self.assertEqual(expected_birth_year, proband[index])
-            else:
-                self.assertEqual(0, proband[index])
+        
+        self.assertEqual(proband[target_idx], 1)
 
-        proband[9] = 65
-        self.assertIn(proband[9], self.pro_age_range)
-        proband[9] = 20
-        self.assertIn(proband[9], self.pro_age_range)
-        proband[9] = 66
-        self.assertNotIn(proband[9], self.pro_age_range)
-        proband[9] = 19
-        self.assertNotIn(proband[9], self.pro_age_range)
-
+        self.assertIn(proband[age_idx], range(20, 66))
+        
     def test_add_parents(self):
         '''
         Tests that:
-        1. TypeError is raised when person already has parents
-        2. Gender for each parent is correct
+        1. ValueError is raised when person already has parents
+        2. Sex of each parent is correct
         3. FamID for person and parents is equal
-        4. Name and IndivID are greater than zero and are equal
-        5. Age in correct range based on child's age
-        6. Birth year set correctly based on age
-        7. Parent status changed to dead if dead
-        8. All other variables set to zero
+        4. Target status is set to 0
+        5. Dead status set correctly based on parent's age
+        6. Birth year is set correctly based on age and dead status
+        7. Age is in correct range based on child's age
         '''
         ped = Pedigree()
-        self.person[4] = 2 
-        self.person[5] = 3
-        with self.assertRaises(TypeError):
+        self.person[fathID_idx] = 2 
+        self.person[mothID_idx] = 3
+        #ensures that a person is not assigned multiple sets of parents
+        #fails if a person who already has parents is used as the arguement in add_parents
+        with self.assertRaises(ValueError):
             parents = ped.add_parents(self.person)
-
-        self.person[4] = 0
-        self.person[5] = 0
+            
+        self.person[fathID_idx] = 0
+        self.person[mothID_idx] = 0
         parents = ped.add_parents(self.person)
 
-        self.assertEqual(parents[0][6], "M")
-        self.assertEqual(parents[1][6], "F")
+        #position of each parent in list parents
+        fathPosition = 0
+        mothPosition = 1
         
-        for parent in parents:
-            for name, index in self.variable_defs.iteritems():
-                if name in self.variable_zero:
-                    self.assertEqual(0, parent[index])
-                elif name == "FamID":
-                    self.assertEqual(self.person[0], parent[index])
-                elif name == "Name":
-                    self.assertGreaterEqual(parent[index], 1)
-                elif name == "IndivID":
-                    self.assertGreaterEqual(parent[index], 1)
-                    self.assertEqual(parent[index], parent[index-2])
-                elif name == "Sex":
-                    self.assertIn(parent[index], self.gender)
-                elif name =="Age":
-                    person_age = self.person[index]
-                    self.assertIn(parent[index], range(person_age + 20, person_age + 41))
-                elif name == "Birth Year":
-                    expected_birth_year = int(year) - int(parent[index-1])
-                    self.assertEqual(expected_birth_year, parent[index])
-                elif name == "Dead":
-                    if parent[index+1] > 90:
-                        self.assertEqual(1, parent[index])
-                    else:
-                        self.assertEqual(0, parent[index])
-                else:
-                    self.assertEqual(0, parent[index])
+        self.assertEqual(parents[fathPosition][sex_idx], "M")
+        self.assertEqual(parents[mothPosition][sex_idx], "F")
 
-        
+        for parent in parents:
+            self.assertEqual(parent[famID_idx], self.person[famID_idx])
+            
+            self.assertEqual(parent[target_idx], 0)
+            
+            if parent[age_idx] > 90:
+                self.assertEqual(parent[dead_idx], 1)
+            else:
+                self.assertEqual(parent[dead_idx], 0)
+                self.assertEqual(parent[birthYear_idx], int(year) - int(parent[age_idx]))
+
+            self.assertIn(parent[age_idx], range(self.person[age_idx] + 20, self.person[age_idx] + 41))
+
+        self.person[fathID_idx] = 0
+        self.person[mothID_idx] = 0
+        #ensures that parent age must be greater than 90
+        self.person[age_idx] = 71
+        parents = ped.add_parents(self.person)
+
+        #checking to make sure that if parent is dead, birth year is correct for actual year born
+        for parent in parents:
+            self.assertEqual(parent[dead_idx], 1)
+            self.assertLessEqual(parent[birthYear_idx], int(year) - int(parent[age_idx]))
+       
     def test_add_partner(self):
         '''
+        Fix to match format in above tests
+
         Tests that:
         1. Gender of partner is correct
         2. FamID for person and partner is equal
@@ -190,6 +153,8 @@ class test_pedigree_methods(unittest.TestCase):
 
     def test_add_offspring(self):
         '''
+        Fix to match format above
+        
         Tests that:
         1. FathID and MothID for each child is set correctly based on gender
         2. FamID set correctly for each child
@@ -244,6 +209,8 @@ class test_pedigree_methods(unittest.TestCase):
                     
     def test_add_siblings(self):
         '''
+        Fix to match new format
+         
         Tests that:
         1. Person has parents or raises ValueError
         2. FamID is equal for person and siblings
@@ -297,6 +264,8 @@ class test_pedigree_methods(unittest.TestCase):
 
     def test_make_healthy_pedigree(self):
         '''
+        Fix to match new test format
+ 
         Tests that:
         1. FamID same for every person in pedigree
         '''
@@ -305,9 +274,8 @@ class test_pedigree_methods(unittest.TestCase):
         pedigree = ped.make_healthy_pedigree()
 
         famID = pedigree[0][0]
-        
+       
         for person in pedigree:
             self.assertEqual(person[0], famID)
-        
-                
+                        
             
